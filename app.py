337fe4137e166a72c64c4e2557a4bb759a6d7063
Simon Hopkins <sgchopkins8043@gmail.com>
2022-03-08 11:29:54 +0000

    few more functions

--- app.py
+++ app.py
@@ -1,27 +1,34 @@
 """
 achievement app
 """
-from modules import Achievement
-import pandas as pd
 import os as __os
 import platform as __platform
 from datetime import datetime as dt
 from datetime import timedelta as td
+import pandas as pd
+from modules import Achievement
+
 
 def open_dataframe():
+    """
+    Function to open the dataframe if it exists, or create a new one if it does not
+    :return: Dataframe
+    """
     print("Checking for presence of data file......")
     try:
         datafile = './data/data.csv'
-        df = pd.read_csv(datafile)
+        dataframe = pd.read_csv(datafile)
         print("File found.... loading into dataframe")
-        return df
+        return dataframe
 
     except IOError:
         if input("File not found: Create new file? (Y/N)").lower() == 'y':
             initial = [Achievement('category field', 'team involved',
                                    'description of achievement', 'context of the report')]
-            df = pd.DataFrame([vars(t) for t in initial])
-            df.to_csv('./data/data.csv', index=False)
+            dataframe = pd.DataFrame([vars(t) for t in initial])
+            dataframe.to_csv('./data/data.csv', index=False)
+            return dataframe
+    return dataframe
 
 
 def clear_screen():
@@ -40,49 +47,74 @@ def clear_screen():
         print("Your OS doesn't seem to be supported!")
 
 
-def print_ten(df):
+def print_ten(dataframe):
+    """
+    Function to print last 10 records from the dataframe
+    :param dataframe:
+    :return: Last 10 records from dataframe
+    """
     pd.options.display.width = 0
-    print(df.tail(10))
+    print(dataframe.tail(10))
 
-def print_last_14_days(df):
+
+def print_last_14_days(dataframe):
+    """
+    Function to print last 14 days worth of records
+    :param dataframe:
+    :return: Last 14 days of records from dataframe
+    """
     pd.options.display.width = 0
-    print(df[df['timestamp'] > (dt.now() - td(days=14)).strftime('%Y-%m-%d %H:%M:%S')])
+    print(dataframe[dataframe['timestamp'] > (dt.now()
+                - td(days=14)).strftime('%Y-%m-%d %H:%M:%S')])
 
-def write_achievement(df):
-    df1 = df.copy()
+
+def write_achievement(dataframe):
+    """
+    Function to write a new achievement to the dataframe
+    :param dataframe:
+    :return: updated dataframe
+    """
+    df_main1 = dataframe.copy()
     category = input("Enter category of achievement: ")
     team = input("Enter team: ")
     achievement = input("Enter achievement: ")
     context = input("Enter Context: ")
     new = Achievement(category, team, achievement, context)
     data = [[new.timestamp, new.category, new.team, new.achievement, new.context]]
-    df2 = pd.DataFrame(data, columns=['timestamp', 'category', 'team', 'achievement', 'context'])
-    df = pd.concat([df1, df2], axis=0)
-    df.to_csv('./data/data.csv', index=False)
-    print(df)
+    df_main2 = pd.DataFrame(data, columns=['timestamp',
+                'category', 'team', 'achievement', 'context'])
+    dataframe = pd.concat([df_main1, df_main2], axis=0)
+    dataframe.to_csv('./data/data.csv', index=False)
+    print(dataframe)
 
-def menu(df):
-    option = input("Enter one of the following options:\n1. List last 10 records...\n2. List previous 14 days...\n3. Choose Time Period..\n4. Write new achievement..\n9. Exit..").lower()
+
+def menu(dataframe):
+    """
+    Function to display menu options and call other functions
+    :param dataframe:
+    :return: dataframe
+    """
+    option = input("Enter one of the following options:\n1. List last 10 records...\n2. "
+                   + "List previous 14 days...\n4. "
+                   + "Write new achievement..\n9. Exit..").lower()
     try:
         int(option)
     except ValueError:
         option = "0"
     if int(option) == 1:
-        print_ten(df)
+        print_ten(dataframe)
+        menu(dataframe)
     elif int(option) == 2:
-        print_last_14_days(df)
-    elif int(option) == 3:
-        print("3")
+        print_last_14_days(dataframe)
+        menu(dataframe)
     elif int(option) == 4:
-        df = write_achievement(df)
-        return df
+        write_achievement(dataframe)
     elif int(option) == 9:
         print("Closing app...")
     else:
         clear_screen()
-        menu()
+        menu(dataframe)
 
-df = open_dataframe()
-menu(df)
-df = pd.read_csv('./data/data.csv')
 
+df_main = open_dataframe()
+menu(df_main)
